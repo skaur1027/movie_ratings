a00a9b41e54b69986733a11975f275d9cb03386b
skaur1027 <malhotra.sandeepk@gmail.com>
2022-04-28 12:13:31 -0700

    second commit

--- crud.py
+++ crud.py
@@ -19,6 +19,16 @@ def create_movie(title, overview, release_date, poster_path):
     return movie
 
 
+def get_movies():
+    all_movies = Movie.query.all()
+    return all_movies
+
+
+def get_movie_by_id(movie_id):
+    movie = Movie.query.get(movie_id)
+    return movie
+
+
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
@@ -26,6 +36,19 @@ def create_rating(user, movie, score):
 
     return rating
 
+def get_users():
+    all_users = User.query.all()
+    return all_users
+
+
+def get_user_by_id(user_id):
+    user = User.query.get(user_id)
+    return user
+
+def get_user_by_email(email):
+    """Show details on a particular user."""
+    
+    return User.query.filter(User.email == email).first()
 
 if __name__ == '__main__':
     from server import app
--- server.py
+++ server.py
@@ -1,14 +1,75 @@
 """Server for movie ratings app."""
 
+from turtle import home
 from flask import Flask
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
+from model import connect_to_db, db
+import crud
+
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
-# Replace this with routes and view functions!
+@app.route('/')
+def homepage():
+    """View homepage."""
 
+    return render_template('homepage.html')
+
+
+@app.route('/movies')
+def movies():
+    """View all movies."""
+    
+    movies = crud.get_movies()
+    return render_template('all_movies.html', movies=movies)
+
+
+@app.route("/movies/<movie_id>")
+def show_movie(movie_id):
+    """Show details on a particular movie."""
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template('movie_details.html', movie=movie)
+
+@app.route('/users')
+def users():
+    """View all users."""
+
+    users = crud.get_users()
+    return render_template('all_users.html', users=users)
+
+
+@app.route("/users/<user_id>")
+def show_user(user_id):
+    """Show details on a particular user."""
+
+    user = crud.get_user_by_id(user_id)
+
+    return render_template('user_details.html', user=user)
+
+@app.route('/users', methods=['POST'])
+def reg_users():
+    """New user can register."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = crud.get_user_by_email(email)
+    if user:
+        flash("Cannot create an account with that email. Try again.")
+    else:
+        user = crud.create_user(email, password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created! Please log in.")
+
+    return redirect("/")
 
 if __name__ == "__main__":
-    # DebugToolbarExtension(app)
-
-    app.run(host="0.0.0.0", debug=True)
+    connect_to_db(app)
+    app.run(debug=True)
